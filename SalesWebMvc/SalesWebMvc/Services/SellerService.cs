0cd8794919e9dc9af571d1256e64dca8da85641b
Rayan <rayancardoso40@gmail.com>
2023-05-27 16:48:58 -0300

    Formulário de Create Simples

---
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
