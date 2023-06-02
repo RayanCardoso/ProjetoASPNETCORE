9ac1703c9df929fd80996634e46698577946e28a
Rayan <rayancardoso40@gmail.com>
2023-06-01 22:44:14 -0300

    Implementando Busca Agrupada

--- SalesWebMvc/SalesWebMvc/Startup.cs
+++ SalesWebMvc/SalesWebMvc/Startup.cs
@@ -34,9 +34,9 @@ namespace SalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
-
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
